fe4ee245040573d5fafc2cfe9102fdf8a3861420
Katalyst99 <katleho333@gmail.com>
2024-05-18 06:43:35 -0700

    Dealing with circular import

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,8 +30,8 @@ class FileStorage():
     def reload(self):
         """Deserializes the JSON file to __objects only if the file exists"""
         from models.base_model import BaseModel
-
         c_dict = {'BaseModel': BaseModel}
+
         try:
             with open(self.__file_path, 'r', encoding='utf-8') as afile:
                 obj_d = json.loads(afile)
@@ -39,4 +39,4 @@ class FileStorage():
                     c_name = c_dict[val['__class__']](**val)
                     self.__objects[key] = c_name
         except FileNotFoundError:
-            pass
+            return
