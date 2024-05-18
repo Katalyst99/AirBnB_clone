0dc2e639e009dafbc435e1824b42764b820f27d1
Katalyst99 <katleho333@gmail.com>
2024-05-18 10:33:01 -0700

    Updated Storage to handle User

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,7 +16,7 @@ class FileStorage():
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
         if obj:
-            key = f"{obj.__class__.__name__}{obj.id}"
+            key = f"{obj.__class__.__name__}.{obj.id}"
             self.__objects[key] = obj
 
     def save(self):
@@ -30,8 +30,9 @@ class FileStorage():
     def reload(self):
         """Deserializes the JSON file to __objects only if the file exists"""
         from models.base_model import BaseModel
+        from models.user import User
+        c_dict = {'BaseModel': BaseModel, 'User': User}
 
-        c_dict = {'BaseModel': BaseModel}
         try:
             with open(self.__file_path, 'r', encoding='utf-8') as afile:
                 obj_d = json.load(afile)
