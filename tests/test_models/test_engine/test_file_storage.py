0ab49d63a357d1ce2993aa55e5741662a4552782
Katalyst99 <katleho333@gmail.com>
2024-05-20 01:51:46 -0700

    JSON file added

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Unittest module for the FileStorage class"""
+import unittest
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
+class TestFileStorage(unittest.Testcase):
+    """Unittests for testing FileStorage"""
+    def setUp(self):
+        """Set up of instances"""
+        self.my_storage = FileStorage()
+
+    def test_FileStorage_instance(self):
+        """Test for is instance"""
+        self.assertIsInstance()
+
+
+
