68cd875cb8e0ebdae7365f221fc4a0a06c0bb74a
Katalyst99 <katleho333@gmail.com>
2024-05-28 10:42:34 -0700

    Completion of file storage tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,15 +5,23 @@ from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
 
-class TestFileStorage(unittest.Testcase):
+class TestFileStorage(unittest.TestCase):
     """Unittests for testing FileStorage"""
     def setUp(self):
         """Set up of instances"""
         self.my_storage = FileStorage()
+        self.my_model = BaseModel()
 
     def test_FileStorage_instance(self):
         """Test for is instance"""
-        self.assertIsInstance()
+        self.assertIsInstance(self.my_storage, FileStorage)
+
+    def test_FileStorage_save(self):
+        """Test for save method instance"""
+        prime_updated_at = self.my_model.updated_at
+        self.my_model.save()
+        self.assertNotEqual(prime_updated_at, self.my_model.updated_at)
 
 
-
+if __name__ == '__main__':
+    unittest.main()
