e0b210390b0e4350a41c1b5baf6bfac05e07e222
Katalyst99 <katleho333@gmail.com>
2024-06-01 08:31:18 -0700

    User test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 """The Unittest module for User"""
+import unittest
+from models.user import User
+
+class TestUser(unittest.TestCase):
+    """Unittests for testing User class"""
