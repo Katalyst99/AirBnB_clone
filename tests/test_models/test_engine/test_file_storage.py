9bb72ba4c1c2dd376c286154849cfb25ec0cf411
Katalyst99 <katleho333@gmail.com>
2024-06-06 07:31:33 -0700

    File storage test 0606

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -10,11 +10,21 @@ class TestFileStorage(unittest.TestCase):
     def setUp(self):
         """Set up of instances"""
         self.my_storage = FileStorage()
+        self.my_model = BaseModel()
+        self.my_model_id = my_model.__class__.name__ + '.' + self.my_model.id
 
-    def test_FileStorage_instance(self):
-        """Test for is instance"""
-        self.assertIsInstance(self.my_storage, FileStorage)
+    def tearDown(self):
+        """Class method to close test case environment"""
+        FileStorage._FileStorage__objects = {}
 
+    def test_FileStorage_all(self):
+        """Test for all method"""
+        self.assertIsInstance(self.my_storage.all(), dict)
+
+    def test_FileStorage_new(self):
+        """Test for new method"""
+        self.storage.new(self.my_model)
+        self.assertIn(self.my_model_id, self.my_storage.all())
     def test_FileStorage_save(self):
         """Test for save method"""
 
