eb7c671fc42827ebc4a6d2b853efc6504cd88121
Katalyst99 <katleho333@gmail.com>
2024-06-09 08:21:51 -0700

    User tests 0609

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,3 +5,20 @@ from models.user import User
 
 class TestUser(unittest.TestCase):
     """Unittests for testing User class"""
+
+    my_user = User()
+
+    def test_is_user(self):
+        """Test for inheritance"""
+        self.assertIsInstance(self.my_user, User)
+
+    def test_user_attributes(self):
+        """Test User class atributes"""
+        self.assertEqual(self.my_user.email, "")
+        self.assertEqual(self.my_user.password, "")
+        self.assertEqual(self.my_user.first_name, "")
+        self.assertEqual(self.my_user.last_name, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
