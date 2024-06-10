60e5b5c7a01b6be619dddb8d52c9c0ee04e02c2b
Katalyst99 <katleho333@gmail.com>
2024-06-10 00:13:57 -0700

    Unittests for review class

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
 """The Unittest module for Review"""
 import unittest
+from models.review import Review
+
+
+class TestReview(unittest.TestCase):
+    """Unittests for testing Review class"""
+
+    my_review = Review()
+
+    def test_is_review(self):
+        """Test for inheritance"""
+        self.assertIsInstance(self.my_review, Review)
+
+    def test_review_attributes(self):
+        """Test Review class atributes"""
+        self.assertEqual(self.my_review.place_id, "")
+        self.assertEqual(self.my_review.user_id, "")
+        self.assertEqual(self.my_review.text, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
