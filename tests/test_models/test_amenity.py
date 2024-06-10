0576b115012b585a158c3b15c47951dc43c7dfb8
Katalyst99 <katleho333@gmail.com>
2024-06-09 23:08:27 -0700

    Unittests for city and amenity class

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 """The Unittest module for Amenity"""
 import unittest
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """Unittests for testing Amenity class"""
+
+    my_amenity = Amenity()
+
+    def test_is_amenity(self):
+        """Test for inheritance"""
+        self.assertIsInstance(self.my_amenity, Amenity)
+
+    def test_amenity_attributes(self):
+        """Test amenity class atributes"""
+        self.assertEqual(self.my_amenity.name, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
 """The Unittest module for City"""
 import unittest
+from models.city import City
+
+
+class TestCity(unittest.TestCase):
+    """Unittests for testing City class"""
+
+    my_city = City()
+
+    def test_is_user(self):
+        """Test for inheritance"""
+        self.assertIsInstance(self.my_city, City)
+
+    def test_city_attributes(self):
+        """Test User class atributes"""
+        self.assertEqual(self.my_city.state_id, "")
+        self.assertEqual(self.my_city.name, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
