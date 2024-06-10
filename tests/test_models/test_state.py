dac7f275f12f48735c1db627a365770b38dc531a
Katalyst99 <katleho333@gmail.com>
2024-06-10 00:23:40 -0700

    Unittests for State class

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 """The Unittest module for State"""
 import unittest
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """Unittests for testing State class"""
+
+    my_state = State()
+
+    def test_is_state(self):
+        """Test for inheritance"""
+        self.assertIsInstance(self.my_state, State)
+
+    def test_state_attributes(self):
+        """Test State class atributes"""
+        self.assertEqual(self.my_state.name, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
